cdc4d1e4f55d2995560427378dc33ca912771f51
Benjamin Ennis <ennis.benjamin.d@gmail.com>
2018-04-04 14:41:45 -0400

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,8 @@ Rails.application.configure do
   config.webpacker.check_yarn_integrity = false
 
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.serve_static_assets = true
+  config.assets.compile = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
